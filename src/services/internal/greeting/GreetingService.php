6b160e6ff9685e1995252c2d2bb4bacbd926b831
Csizmarik Norbert <Csizmarik.Norbert@biztositas.hu>
2017-03-24 15:33:54 +0100

    DI konkrét típus nélkül
    Signed-off-by: Csizmarik Norbert <Csizmarik.Norbert@biztositas.hu>

--- src/services/internal/greeting/GreetingService.php
+++ src/services/internal/greeting/GreetingService.php
@@ -8,7 +8,6 @@
 
 namespace services\internal\greeting;
 
-use services\internal\day_period\DayPeriodService;
 use services\internal\greeting\executing_task\GreetingForDayPeriod;
 
 class GreetingService
@@ -18,7 +17,7 @@ class GreetingService
      */
     private $dayPeriodService;
 
-    public function __construct(DayPeriodService $dayPeriodService)
+    public function __construct($dayPeriodService)
     {
         $this->dayPeriodService = $dayPeriodService;
     }
@@ -26,6 +25,8 @@ class GreetingService
     public function greetingAt(\DateTime $dateTime)
     {
         $dayPeriod = $this->dayPeriodService->getDayPeriodForTime($dateTime);
+        // NOT TODO - nem módosíthatok állapotot itt sem
+		//$dayPeriod->var = 10;
         $greetingForTime = new GreetingForDayPeriod();
         return $greetingForTime->greetinForDayPeriod($dayPeriod);
     }
--- tests/services/internal/day_period/executing_task/TimeToDayPeriodTest.php
+++ tests/services/internal/day_period/executing_task/TimeToDayPeriodTest.php
@@ -15,16 +15,10 @@ class TimeToDayPeriodTest extends TestCase
 {
     public function dayPeriodProvider() {
         return [
-            'morning test 1' => [8, 10, DayPeriod::MORNING],
             'last minute of morning' => [9, 59, DayPeriod::MORNING],
             'first minute of day' => [10, 0, DayPeriod::DAY],
-            'day test 1' => [10, 20, DayPeriod::DAY],
-            'day test 2' => [12, 0, DayPeriod::DAY],
-            'day test 3' => [16, 23, DayPeriod::DAY],
             'last minute of the day' => [19, 59, DayPeriod::DAY],
             'first minute of the evening' => [20, 0, DayPeriod::EVENING],
-            'evening test 1' => [23, 59, DayPeriod::EVENING],
-            'evening test 2' => [0, 0, DayPeriod::EVENING],
             'last minute of the evening' => [4, 59, DayPeriod::EVENING],
             'first minute of morning' => [5, 0, DayPeriod::MORNING]
         ];
